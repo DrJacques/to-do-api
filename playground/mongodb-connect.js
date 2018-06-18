513b83043f12033a1728444dfafd713635927723
DrJacques <jacquesiragena@yahoo.com>
2018-06-18 13:57:40 +0200

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,40 @@
+const MongoClient = require('mongodb').MongoClient;
+// const { MongoClient, ObjectID } = require('mongodb');
+
+
+MongoClient.connect('mongodb://127.0.0.1:27017/TodosApp',(err,db)=>{
+    if(err){
+        return console.log('unable to connect to mongodb server');
+    }
+
+    console.log('Connected successfully to the database server!');
+
+    // db.collection('Todos').insertOne({
+    //     text:'Something to do',
+    //     completed: false
+    // },(err,result)=>{
+    //     if(err){
+    //         return console.log('unable to insert todo',err);
+    //     }
+    //     console.log(JSON.stringify(result.ops,undefined,2));
+    // });
+
+    // db.collection('Users').insertOne({
+    //    // _id: 112,
+    //     name:'Jacques',
+    //     age: 27,
+    //     location: 'Kigali - Rwanda'
+
+    // },(err,result)=>{
+    //     if(err){
+    //         return console.log('Unable to insert a record',err);
+    //     }
+
+    //     console.log(result.ops[0]._id.getTimestamp());
+
+    // })
+
+
+    // db.close();
+
+});
